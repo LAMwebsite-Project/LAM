4da5a4aa651647e44ade0528f01b867643f849d3
Zoeezo <zoey.eijgenberger.2004@gmail.com>
2021-01-05 20:19:46 +0100

    language helper geupdate

--- helpers/language.js
+++ helpers/language.js
@@ -1,31 +1,33 @@
 const acceptedLanguages = ['en', 'nl'];
 
-function checkLanguage(req) {
+function checkLanguage(req, res) {
     let cookie = req.cookies['lang'];
     let accepted = false;
     
     for(lang in acceptedLanguages) {
-        if(cookie == lang) {
+        if(cookie == acceptedLanguages[lang]) {
             accepted = true;
+            console.log('ok');
         }
     }
 
     if(cookie == undefined || accepted == false) {
-        cookie = setLanguage(req);
+        cookie = setLanguage(req, res);
+        console.log('wow')
     }
 
     return cookie.toUpperCase();
 }
 
-function setLanguage(req) {
+function setLanguage(req, res) {
     for(lang in req.acceptsLanguages()) {
         if(lang == 'nl') {
-            res.cookie('lang', 'nl', {expires: 2147483647});
+            res.cookie('lang', 'nl', {maxAge: 604800000});
             return 'nl';
         }
     }
 
-    res.cookie('lang', 'en', {expires: 2147483647});
+    res.cookie('lang', 'en', {maxAge: 604800000});
     return 'en';
 }
 

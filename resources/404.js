c482d85196397231ad185126d0414870e4eacfb1
Zoeezo <zoey.eijgenberger.2004@gmail.com>
2021-01-05 20:57:07 +0100

    404 pagina geupdate

---
+++ resources/404.js
@@ -0,0 +1,6 @@
+const imagewrapper = document.getElementById('image-wrapper');
+const image = document.getElementById('image');
+
+imagewrapper.addEventListener('click', () => {
+    image.src = '/resources/404_2.png';
+});
